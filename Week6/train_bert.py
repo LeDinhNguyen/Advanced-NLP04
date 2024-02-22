85c30dd5ab0904db7013e612ff737ac991297e85
LeDinhNguyen <ledinhnguyen00@gmail.com>
2024-02-22 20:55:23 +0700

    uncomment code

--- Week6/train_bert.py
+++ Week6/train_bert.py
@@ -65,58 +65,58 @@ eval_dataloader = DataLoader(
     },
 )
 
-# mixed_precision_dtype = None # torch.bfloat16
-# ctx = nullcontext() if mixed_precision_dtype == None else torch.amp.autocast(device_type='cuda', dtype=mixed_precision_dtype)
-# # Initialize GradScaler for mixed precision training
-# scaler = GradScaler()
+mixed_precision_dtype = None # torch.bfloat16
+ctx = nullcontext() if mixed_precision_dtype == None else torch.amp.autocast(device_type='cuda', dtype=mixed_precision_dtype)
+# Initialize GradScaler for mixed precision training
+scaler = GradScaler()
 
-# for epoch in range(3):
-#     # Training
-#     model.train()
-#     total_train_loss, total_train_correct = 0, 0
-#     train_progress_bar = tqdm(train_dataloader, desc=f"Epoch {epoch + 1} [Training]", position=0, leave=True)
-#     for batch in train_progress_bar:
-#         input_ids, attention_masks, labels = (batch["input_ids"].to(device), batch["attention_mask"].to(device), batch["labels"].to(device))
+for epoch in range(3):
+    # Training
+    model.train()
+    total_train_loss, total_train_correct = 0, 0
+    train_progress_bar = tqdm(train_dataloader, desc=f"Epoch {epoch + 1} [Training]", position=0, leave=True)
+    for batch in train_progress_bar:
+        input_ids, attention_masks, labels = (batch["input_ids"].to(device), batch["attention_mask"].to(device), batch["labels"].to(device))
 
-#         optimizer.zero_grad()
+        optimizer.zero_grad()
 
-#         # Use autocast to automatically cast tensor types for mixed precision training
-#         with ctx:
-#             outputs = model(input_ids, attention_mask=attention_masks, labels=labels)
-#             loss = criterion(outputs.logits, labels)
-#         if mixed_precision_dtype:
-#             # Scale the loss and backpropagate with the help of GradScaler
-#             scaler.scale(loss).backward()
-#             scaler.step(optimizer)
-#             scaler.update()
-#         else:
-#             loss.backward()
-#             optimizer.step()
+        # Use autocast to automatically cast tensor types for mixed precision training
+        with ctx:
+            outputs = model(input_ids, attention_mask=attention_masks, labels=labels)
+            loss = criterion(outputs.logits, labels)
+        if mixed_precision_dtype:
+            # Scale the loss and backpropagate with the help of GradScaler
+            scaler.scale(loss).backward()
+            scaler.step(optimizer)
+            scaler.update()
+        else:
+            loss.backward()
+            optimizer.step()
 
-#         total_train_loss += loss.item()
-#         preds = torch.argmax(outputs.logits, dim=1)
-#         total_train_correct += (preds == labels).sum().item()
+        total_train_loss += loss.item()
+        preds = torch.argmax(outputs.logits, dim=1)
+        total_train_correct += (preds == labels).sum().item()
         
-#     avg_train_loss = total_train_loss / len(train_dataloader)
-#     avg_train_accuracy = total_train_correct / len(encoded_train_dataset)
-#     print(f"Epoch {epoch + 1}, Train Loss: {avg_train_loss}, Train Accuracy: {avg_train_accuracy}")
+    avg_train_loss = total_train_loss / len(train_dataloader)
+    avg_train_accuracy = total_train_correct / len(encoded_train_dataset)
+    print(f"Epoch {epoch + 1}, Train Loss: {avg_train_loss}, Train Accuracy: {avg_train_accuracy}")
 
     
-#     # Evaluation
-#     model.eval()
-#     total_eval_loss, total_eval_correct = 0, 0
-#     eval_progress_bar = tqdm(eval_dataloader, desc=f"Epoch {epoch + 1} [Evaluation]", position=0, leave=True)
-#     for batch in eval_progress_bar:
-#         input_ids, attention_masks, labels = (batch["input_ids"].to(device), batch["attention_mask"].to(device), batch["labels"].to(device))
+    # Evaluation
+    model.eval()
+    total_eval_loss, total_eval_correct = 0, 0
+    eval_progress_bar = tqdm(eval_dataloader, desc=f"Epoch {epoch + 1} [Evaluation]", position=0, leave=True)
+    for batch in eval_progress_bar:
+        input_ids, attention_masks, labels = (batch["input_ids"].to(device), batch["attention_mask"].to(device), batch["labels"].to(device))
 
-#         with torch.no_grad():
-#             with ctx:
-#                 outputs = model(input_ids, attention_mask=attention_masks, labels=labels)
-#                 loss = criterion(outputs.logits, labels)
-#         total_eval_loss += loss.item()
-#         preds = torch.argmax(outputs.logits, dim=1)
-#         total_eval_correct += (preds == labels).sum().item()
+        with torch.no_grad():
+            with ctx:
+                outputs = model(input_ids, attention_mask=attention_masks, labels=labels)
+                loss = criterion(outputs.logits, labels)
+        total_eval_loss += loss.item()
+        preds = torch.argmax(outputs.logits, dim=1)
+        total_eval_correct += (preds == labels).sum().item()
 
-#     avg_eval_loss = total_eval_loss / len(eval_dataloader)
-#     avg_eval_accuracy = total_eval_correct / len(encoded_eval_dataset)
-#     print(f"Epoch {epoch + 1}, Evaluation Loss: {avg_eval_loss}, Evaluation Accuracy: {avg_eval_accuracy}")
+    avg_eval_loss = total_eval_loss / len(eval_dataloader)
+    avg_eval_accuracy = total_eval_correct / len(encoded_eval_dataset)
+    print(f"Epoch {epoch + 1}, Evaluation Loss: {avg_eval_loss}, Evaluation Accuracy: {avg_eval_accuracy}")
